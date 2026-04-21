cedc7fd6b9dca5d93759c9c5ee76f535b5387f37
baohuy1303 <huynhbaohuy130333@gmail.com>
2026-04-21 00:48:03 -0500

    feat: initialize project with scene, cup script, and particle system prefab

--- Assets/Scripts/Cup.cs
+++ Assets/Scripts/Cup.cs
@@ -8,6 +8,7 @@ public class Cup : MonoBehaviour
     [Tooltip("Drag the BoxCollider2D (the trigger zone) here")]
     [SerializeField] private Collider2D scoreTriggerCollider;
     private int score = 0;
+    public GameObject particleSystem;
     void Start()
     {
         UpdateScoreText();
@@ -17,6 +18,7 @@ public class Cup : MonoBehaviour
         // 1. Check if the object colliding is our projectile
         NumericalProjectile proj = collision.GetComponent<NumericalProjectile>();
         if (proj == null) return;
+        Instantiate(particleSystem, transform.position, Quaternion.identity);
         // 2. Check if the collision happened specifically with our designated score trigger
         // This ignores your Polygon Collider or any other colliders on this object.
         if (scoreTriggerCollider != null && !collision.IsTouching(scoreTriggerCollider))
@@ -29,6 +31,7 @@ public class Cup : MonoBehaviour
         Respawn();
         
         Destroy(collision.gameObject);
+        
     }
 
     private void UpdateScoreText()
